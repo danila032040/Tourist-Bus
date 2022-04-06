c3496d5d17a8b3f647711725374dfac31fb14193
Даниил Радченко <37844948+danila032040@users.noreply.github.com>
2022-04-06 21:01:12 +0200

    Added some windows.

---
+++ TouristBusApp/Windows/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace TouristBusApp.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
