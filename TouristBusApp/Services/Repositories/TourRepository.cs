ac2bcb1989d04e67190eec33eaa44c1c012d12b6
Даниил Радченко <37844948+danila032040@users.noreply.github.com>
2022-04-07 18:24:59 +0200

    Writed 2 Repositories for Tour and Users

--- TouristBusApp/Services/Repositories/TourRepository.cs
+++ TouristBusApp/Services/Repositories/TourRepository.cs
@@ -1,28 +1,60 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
 using TouristBusApp.Models;
 
 namespace TouristBusApp.Services.Repositories
 {
     public class TourRepository : IRepository<Tour>
     {
+        private readonly string _fileName;
+
+        public TourRepository(string fileName)
+        {
+            _fileName = $@"{Directory.GetCurrentDirectory()}\{fileName}";
+        }
         public void Create(Tour entity)
         {
-            throw new System.NotImplementedException();
+            string json = File.ReadAllText(_fileName);
+            var toursInFile = JsonConvert.DeserializeObject<List<Tour>>(json);
+
+            while(toursInFile.Any(t=>t.Id == entity.Id)) entity.Id = new Random().Next();
+            
+            toursInFile.Add(entity);
+            json = JsonConvert.SerializeObject(toursInFile);
+            File.WriteAllText(_fileName, json);
         }
 
         public IEnumerable<Tour> Read()
         {
-            throw new System.NotImplementedException();
+            string json = File.ReadAllText(_fileName);
+            return JsonConvert.DeserializeObject<IEnumerable<Tour>>(json);
         }
 
         public void Update(Tour entity)
         {
-            throw new System.NotImplementedException();
+            string json = File.ReadAllText(_fileName);
+            var toursInFile = JsonConvert.DeserializeObject<List<Tour>>(json);
+
+            int index = toursInFile.FindIndex(t => t.Id == entity.Id);
+
+            toursInFile[index] = entity;
+            
+            json = JsonConvert.SerializeObject(toursInFile);
+            File.WriteAllText(_fileName, json);
         }
 
         public void Delete(int id)
         {
-            throw new System.NotImplementedException();
+            string json = File.ReadAllText(_fileName);
+            var toursInFile = JsonConvert.DeserializeObject<List<Tour>>(json);
+
+            toursInFile.RemoveAt(toursInFile.FindIndex(t => t.Id == id));
+            
+            json = JsonConvert.SerializeObject(toursInFile);
+            File.WriteAllText(_fileName, json);
         }
     }
 }
--- TouristBusApp/Services/Repositories/UserRepository.cs
+++ TouristBusApp/Services/Repositories/UserRepository.cs
@@ -1,28 +1,60 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
 using TouristBusApp.Models;
 
 namespace TouristBusApp.Services.Repositories
 {
     public class UserRepository : IRepository<User>
     {
+        private readonly string _fileName;
+
+        public UserRepository(string fileName)
+        {
+            _fileName = $@"{Directory.GetCurrentDirectory()}\{fileName}";
+        }
         public void Create(User entity)
         {
-            throw new System.NotImplementedException();
+            string json = File.ReadAllText(_fileName);
+            var usersInFile = JsonConvert.DeserializeObject<List<User>>(json);
+
+            while(usersInFile.Any(t=>t.Id == entity.Id)) entity.Id = new Random().Next();
+            
+            usersInFile.Add(entity);
+            json = JsonConvert.SerializeObject(usersInFile);
+            File.WriteAllText(_fileName, json);
         }
 
         public IEnumerable<User> Read()
         {
-            throw new System.NotImplementedException();
+            string json = File.ReadAllText(_fileName);
+            return JsonConvert.DeserializeObject<IEnumerable<User>>(json);
         }
 
         public void Update(User entity)
         {
-            throw new System.NotImplementedException();
+            string json = File.ReadAllText(_fileName);
+            var usersInFile = JsonConvert.DeserializeObject<List<User>>(json);
+
+            int index = usersInFile.FindIndex(t => t.Id == entity.Id);
+
+            usersInFile[index] = entity;
+            
+            json = JsonConvert.SerializeObject(usersInFile);
+            File.WriteAllText(_fileName, json);
         }
 
         public void Delete(int id)
         {
-            throw new System.NotImplementedException();
+            string json = File.ReadAllText(_fileName);
+            var usersInFile = JsonConvert.DeserializeObject<List<User>>(json);
+
+            usersInFile.RemoveAt(usersInFile.FindIndex(t => t.Id == id));
+            
+            json = JsonConvert.SerializeObject(usersInFile);
+            File.WriteAllText(_fileName, json);
         }
     }
 }
