9bff65b4cd4ad741ac51b4bc590c990ea3491891
Даниил Радченко <37844948+danila032040@users.noreply.github.com>
2022-04-08 12:07:18 +0200

    Writed functionality for Authorization

--- TouristBusApp/Services/Authorization.cs
+++ TouristBusApp/Services/Authorization.cs
@@ -1,19 +1,33 @@
+using System.Linq;
 using TouristBusApp.Models;
+using TouristBusApp.Services.Repositories;
 
 namespace TouristBusApp.Services
 {
     public class Authorization
     {
+        private readonly IRepository<User> _rep;
+
         private User _signedInUser;
+
+        public Authorization(IRepository<User> rep)
+        {
+            _rep = rep;
+        }
 
         public void SignIn(string login, string password)
         {
-            
+            _signedInUser = _rep.Read().FirstOrDefault((u) => u.Login == login && u.Password == password);
         }
 
         public void SignOut()
         {
             _signedInUser = null;
+        }
+
+        public User GetSignedInUser()
+        {
+            return _signedInUser;
         }
     }
 }
