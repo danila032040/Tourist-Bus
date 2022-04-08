8d5ff8675f8e21ad261cb34a130b81a90d913184
Даниил Радченко <37844948+danila032040@users.noreply.github.com>
2022-04-08 18:36:19 +0200

    Started writing interface for Login and Registor windows.

--- TouristBusApp/Windows/MainWindow.xaml.cs
+++ TouristBusApp/Windows/MainWindow.xaml.cs
@@ -4,9 +4,30 @@ namespace TouristBusApp.Windows
 {
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonSignIn_OnClick(object sender, RoutedEventArgs e)
+        {
+            Window window = new LoginWindow();
+            window.Owner = this;
+            if (window.ShowDialog() == true)
+            {
+                MessageBox.Show("Signed in!!!");
+            }
+        }
+
+        private void ButtonRegister_OnClick(object sender, RoutedEventArgs e)
+        {
+            Window window = new RegisterWindow();
+            window.Owner = this;
+            if (window.ShowDialog() == true)
+            {
+                MessageBox.Show("Registered!!!");
+            }
         }
     }
 }
