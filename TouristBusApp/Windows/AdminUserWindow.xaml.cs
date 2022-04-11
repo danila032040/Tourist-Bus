ce41cfb4deb4b4a25eb72017a44ab08a74cca4b5
Даниил Радченко <37844948+danila032040@users.noreply.github.com>
2022-04-11 10:49:19 +0200

    Started writing interface for Admin and Normal users.

--- TouristBusApp/Windows/AdminUserWindow.xaml.cs
+++ TouristBusApp/Windows/AdminUserWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using TouristBusApp.Resources;
 
 namespace TouristBusApp.Windows
 {
@@ -7,6 +8,14 @@ namespace TouristBusApp.Windows
         public AdminUserWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonSignOut_OnClick(object sender, RoutedEventArgs e)
+        {
+            ProjectResource.Instance.Authentication.SignOut();
+            Application.Current.MainWindow = new AuthenticationWindow();
+            Application.Current.MainWindow.Show();
+            Close();
         }
     }
 }
